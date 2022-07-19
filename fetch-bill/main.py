0d5960b81a5a92b068ab2f81579cad7f308fb717
krzysztow <krzysztow@gmail.com>
2022-07-19 05:20:45 +0100

    Removed deprication and added logger

--- fetch-bill/main.py
+++ fetch-bill/main.py
@@ -1,13 +1,19 @@
 import datetime
+import logging
 import os
 from dataclasses import dataclass
 from datetime import date
 
 import requests
 from selenium import webdriver as wd
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 @dataclass(frozen=True)
@@ -27,7 +33,10 @@ class BillFetcher:
     _driver: WebDriver
 
     def __init__(self, webdriver_location: str):
-        self._driver = wd.Chrome(webdriver_location)
+        opts = Options()
+        opts.add_argument("--headless")
+        srvc = Service(executable_path=webdriver_location)
+        self._driver = wd.Chrome(options=opts, service=srvc)
 
     def __enter__(self):
         return self
@@ -38,11 +47,13 @@ class BillFetcher:
     def accept_cookies(self):
         # TODO: check why we cannot set cookie instead of interacting
         # driver.add_cookie({'name': 'ho_cookie_consent', 'value': 'essential'})
+        logger.info("Accept cookies")
         accept_cookies_form_el = self._driver.find_element(By.ID, 'cookie_policy')
         accept_cookies_buttons_el = accept_cookies_form_el.find_element(By.CLASS_NAME, "accept")
         accept_cookies_buttons_el.click()
 
     def fill_and_submit_login_form(self, username: str, password: str):
+        logger.info("Submitting logging information")
         login_form_el = self._driver.find_element(By.ID, "loginForm")
 
         email_el = login_form_el.find_element(By.ID, "email")
@@ -57,6 +68,7 @@ class BillFetcher:
         def find_column_value_for(col: WebElement, value_description: str) -> str:
             return col.find_element(By.XPATH, f'//p[text()="{value_description}"]/../following-sibling::div[1]/p').text
 
+        logger.info("Retrieve last bill data")
         invoice_date_str = find_column_value_for(right_col, "Last invoice date")
         bi = BillInfo(
             invoice_value=find_column_value_for(right_col, "Current invoice"),
@@ -85,13 +97,17 @@ class BillFetcher:
            </div>
         </div>
         """
+        logger.info("Retrieving PDF")
         right_col.find_element(By.XPATH, "//button[text()='View latest bill']").click()
 
         src = self._driver.find_element(By.ID, 'iframeBill').get_attribute("src")
+        logger.debug(f"Opening PDF source {src}")
         self._driver.get(src)
 
+        logger.debug("Fetching PDF with requests")
         cookies_dict = {c['name']: c['value'] for c in self._driver.get_cookies()}
         response = requests.get(src, cookies=cookies_dict)
+        logger.debug(f"Fetching PDF with requests: {response.status_code}")
         with open(out_pdf_path, mode="wb") as f:
             f.write(response.content)
 
@@ -121,9 +137,10 @@ def main():
     username = os.getenv("HO_USERNAME")
     password = os.getenv("HO_PASSWORD")
 
+    logging.info("Starting fetcher...")
     with BillFetcher(webdriver_location) as fetcher:
         result = fetcher.get_last_bill_info(username, password)
-    print(f"Finished successfully: {result}")
+    logging.info(f"Finished successfully: {result}")
 
 
 main()
