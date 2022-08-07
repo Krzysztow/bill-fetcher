de02e711b6389e617667efa05d9dee95dc56c20d
krzysztow <krzysztow@gmail.com>
2022-08-07 20:58:48 +0100

    Sending email with PDF and XLSX claim form

--- src/bill-sender/aws_sender.py
+++ src/bill-sender/aws_sender.py
@@ -10,13 +10,15 @@ import boto3
 from bill_info_result import BillInfoDecoder, BillInfoResult
 from botocore.exceptions import ClientError
 
+from fill_expense_form import ExpenseFormFiller
+
 CHARSET = "UTF-8"
 BUCKET_ARN_REGEX = re.compile(r"""arn:aws:s3:::([^/]*)/(.*)""")
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-boto3.set_stream_logger('', logging.DEBUG)
+# boto3.set_stream_logger('', logging.DEBUG)
 
 
 @dataclass(frozen=True)
@@ -51,7 +53,7 @@ def email_text():
     return """Hello {recipient_name},
 
 Please find attached Broadband Expenses claim form for {billing_period} with receipt.
-As last time, I made sure it's capped at 20£ in the "Total" column and the "Other" column contains real value.
+Please note, I made sure it's capped at 20£ in the "Total" column and the "Other" column contains real value.
 
 Best regards,
 {my_name}""".format(
@@ -61,7 +63,8 @@ Best regards,
     )
 
 
-def send_email(bill_info: BillInfoResult, local_pdf_path: str):
+def send_email(bill_info: BillInfoResult, local_pdf_path: str, local_expenses_path: str):
+    logger.info("Email processing...")
     subject = "Broadband expenses {billing_period}".format(billing_period=bill_info.bill_info.invoice_date)
     recipient = "chriswielgo+ses@gmail.com"
     sender = "chriswielgo+ses@gmail.com"
@@ -75,6 +78,7 @@ def send_email(bill_info: BillInfoResult, local_pdf_path: str):
     part.set_charset(CHARSET)
     message.attach(part)
 
+    logger.info("Attaching pdf...")
     with open(local_pdf_path, 'rb') as pdf:
         part = MIMEApplication(pdf.read(), 'pdf')
         part.add_header(
@@ -83,15 +87,40 @@ def send_email(bill_info: BillInfoResult, local_pdf_path: str):
             filename=f"{bill_info.bill_info.service_name}-{bill_info.bill_info.invoice_date}.pdf")
         message.attach(part)
 
+    # TODO: deduplicate with above
+    logger.info("Attaching xlsx...")
+    with open(local_expenses_path, 'rb') as xlsx:
+        part = MIMEApplication(xlsx.read(), 'vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        part.add_header(
+            'Content-Disposition',
+            'attachment',
+            filename=f"expenses-{bill_info.bill_info.invoice_date}.xlsx")
+        message.attach(part)
+
     ses = boto3.client("ses")
 
+    logger.info("Sending email...")
     response = ses.send_raw_email(
         Destinations=[recipient],
         Source=sender,
         RawMessage={'Data': message.as_string()}
     )
 
-    print("Sent email: ", json.dumps(response))
+    print("Email sent: ", json.dumps(response))
+
+
+def prepare_expenses_form(bill_info: BillInfoResult, output_xls_path: str):
+    logger.info("Filling in the expenses claim form...")
+    filler = ExpenseFormFiller('broadband-expenses.xlsx')
+    filler.fill_in(bill_info)
+    filler.save(output_xls_path)
+
+
+def prepare_bill_pdf(bill_info, local_pdf_path):
+    logger.info("Downloading PDF bill...")
+    pdf_bucket_location = parse_bucket_arn(bill_info.pdf_location)
+    print("Parsed PDF location", pdf_bucket_location)
+    download_from_s3(pdf_bucket_location.bucket_name, pdf_bucket_location.object_path, local_pdf_path)
 
 
 def send_bill(event: dict, _):
@@ -103,9 +132,10 @@ def send_bill(event: dict, _):
 
     print(f"Bill info: {bill_info}")
 
-    pdf_bucket_location = parse_bucket_arn(bill_info.pdf_location)
-    print("Parsed PDF location", pdf_bucket_location)
     local_pdf_path = "/tmp/bill.pdf"
-    download_from_s3(pdf_bucket_location.bucket_name, pdf_bucket_location.object_path, local_pdf_path)
+    prepare_bill_pdf(bill_info, local_pdf_path)
 
-    send_email(bill_info, local_pdf_path)
+    local_expenses_path = "/tmp/expenses.xlsx"
+    prepare_expenses_form(bill_info, local_expenses_path)
+
+    send_email(bill_info, local_pdf_path, local_expenses_path)
